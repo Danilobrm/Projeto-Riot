96d13af26ff7b6713adcd2c147fbd4bcc024efa6
danilobrm <danilobr.web@gmail.com>
2023-02-24 21:02:32 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import GlobalStyle from './styles/GlobalStyle';
+
+import UserContext from './contexts/UserContext';
 
 function App() {
+  const [user, setUser] = useState('');
+  const [pass, setPass] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={{ user, setUser, pass, setPass }}>
+      <BrowserRouter>
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
